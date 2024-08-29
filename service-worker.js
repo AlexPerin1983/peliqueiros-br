f59f7af86f2d83b5f07ad6c5765ccc1c681cea46
AlexPerin1983 <windowfilm.br@gmail.com>
2024-08-29 19:07:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,44 @@
+const CACHE_NAME = 'app-cache-v2'; // Alterar versão quando atualizar a aplicação
+const urlsToCache = [
+    '/peliqueiros-br/',
+    '/peliqueiros-br/index.html',
+    '/peliqueiros-br/icons/menor.png',
+    '/peliqueiros-br/icons/maior.png',
+    '/peliqueiros-br/manifest.json',
+    // Adicione outros recursos necessários para o funcionamento offline aqui
+];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/peliqueiros-br/',
-        '/peliqueiros-br/index.html',
-        '/peliqueiros-br/icons/menor.png',
-        '/peliqueiros-br/icons/maior.png',
-        // Adicione outros recursos necessários para o funcionamento offline aqui
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Arquivos em cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Removendo cache antigo:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
