d1f2141c252cb24ddb7341feb4f5ab1177d4c821
AlexPerin1983 <windowfilm.br@gmail.com>
2024-08-29 20:29:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v10'; // Nome do cache, altere a cada nova versão
+const CACHE_NAME = 'app-cache-v11'; // Nome do cache, altere a cada nova versão
 
 const urlsToCache = [
     '/peliqueiros-br/',
